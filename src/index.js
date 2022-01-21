0078e9fac12394234132688be0adf11faccc86c1
Aliaksandr Pratasau <Aliaksandr.Pratasau@itechart-group.com>
2022-01-21 16:18:43 +0300

    feat(header): add mobile header, header completed

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './assets/styles.scss'
 
 const navbarTemplate = require('./assets/navbar/navbar.handlebars')
 const appointmentSectionTemplate = require('./assets/appointment-section/appointment-section.handlebars')
+const mobileNavbarTemplate = require('./assets/navbar/navbar-mobile.handlebars')
 
 document.addEventListener('DOMContentLoaded', () => {
     const navbar = document.createElement('nav')
@@ -11,20 +12,35 @@ document.addEventListener('DOMContentLoaded', () => {
     })
     navbar.classList.add('navbar')
     document.body.appendChild(navbar)
+
+    const mobileNavbar = document.createElement('nav')
+    mobileNavbar.innerHTML = mobileNavbarTemplate({
+        'navbar-links': {},
+    })
+    mobileNavbar.classList.add('navbar-mobile')
+    document.body.appendChild(mobileNavbar)
+
     const appointmentSection = document.createElement('section')
     appointmentSection.innerHTML = appointmentSectionTemplate({})
     appointmentSection.classList.add('appointment-section')
     document.body.appendChild(appointmentSection)
 
-    function showModal() {
+    function toggleModal() {
         document.getElementById('search-modal').classList.toggle('show')
     }
+    function toggleMobileMenu() {
+        document.getElementById('link-container').classList.toggle('show')
+    }
 
-    document.getElementById('search-btn').addEventListener('click', showModal)
+    document.getElementById('search-btn').addEventListener('click', toggleModal)
     document
         .getElementById('search-submit')
-        .addEventListener('click', showModal)
+        .addEventListener('click', toggleModal)
     document
         .getElementById('close-modal-btn')
-        .addEventListener('click', showModal)
+        .addEventListener('click', toggleModal)
+
+    document
+        .getElementById('menu-btn')
+        .addEventListener('click', toggleMobileMenu)
 })
