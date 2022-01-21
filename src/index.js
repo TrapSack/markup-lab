0f59e99ee52be767e875a529a3bc095b593276a4
Aliaksandr Pratasau <Aliaksandr.Pratasau@itechart-group.com>
2022-01-21 13:54:00 +0300

    feat(header,section1):add modal window on click, appointment section

--- src/index.js
+++ src/index.js
@@ -1,34 +1,30 @@
 import './assets/styles.scss'
 
-// const teamSectionTemplate = require('./assets/team-section/team-section.handlebars')
 const navbarTemplate = require('./assets/navbar/navbar.handlebars')
+const appointmentSectionTemplate = require('./assets/appointment-section/appointment-section.handlebars')
 
 document.addEventListener('DOMContentLoaded', () => {
-    // Team Section component start
-    // const teamSection = document.createElement('section')
-    // teamSection.innerHTML = teamSectionTemplate({
-    //     title: 'Meet Our Team',
-    //     description:
-    //         'Problems trying to resolve the conflict between the two major realms of Classical physics: Newtonian mechanics',
-    //     team: [
-    //         { name: 'Avie Beaton', position: 'CO Founder' },
-    //         { name: 'Ben Jonson', position: 'Consultant' },
-    //         { name: 'Ashley Fletcher', position: 'CEO' },
-    //     ],
-    // })
-    // teamSection.classList.add('team-section')
-    // // Team Section component end
-
-    // document.body.appendChild(teamSection)
-
     const navbar = document.createElement('nav')
-
     navbar.innerHTML = navbarTemplate({
-        'navbar-links': {
-            searchIconPath: './assets/img/search-icon.svg',
-            cardIconPath: './assets/img/card-icon.svg',
-        },
+        'navbar-links': {},
+        'search-modal': {},
     })
     navbar.classList.add('navbar')
     document.body.appendChild(navbar)
+    const appointmentSection = document.createElement('section')
+    appointmentSection.innerHTML = appointmentSectionTemplate({})
+    appointmentSection.classList.add('appointment-section')
+    document.body.appendChild(appointmentSection)
+
+    function showModal() {
+        document.getElementById('search-modal').classList.toggle('show')
+    }
+
+    document.getElementById('search-btn').addEventListener('click', showModal)
+    document
+        .getElementById('search-submit')
+        .addEventListener('click', showModal)
+    document
+        .getElementById('close-modal-btn')
+        .addEventListener('click', showModal)
 })
